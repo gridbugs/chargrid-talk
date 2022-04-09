230fec2beefa50071a3afeb5e006ff578a6dae6c
Stephen Sherratt <stephen@sherra.tt>
2022-04-09 11:37:30 +1000

    Can run in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use chargrid_wgpu::*;
+use chargrid_core::prelude::*;
 use meap::Parser;
 
 const CELL_SIZE: f64 = 48.;
@@ -16,10 +16,29 @@ mod game_component;
 mod main_menu;
 mod menu;
 
-fn main() {
-    let version = meap::prelude::pos_req::<u8>("which version to run")
-        .with_help_default()
-        .parse_env_or_exit();
+struct Args {
+    version: u8,
+    terminal: bool,
+}
+
+impl Args {
+    fn parser() -> impl meap::Parser<Item = Self> {
+        meap::let_map! {
+            let {
+                version = pos_req::<u8>("which version to run");
+                terminal = flag("terminal");
+            } in {
+                Self { version, terminal }
+            }
+        }
+    }
+}
+
+fn run_wgpu<C>(component: C)
+where
+    C: 'static + Component<State = (), Output = app::Output>,
+{
+    use chargrid_wgpu::*;
     let context = Context::new(Config {
         font_bytes: FontBytes {
             normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin-custom.ttf").to_vec(),
@@ -43,13 +62,39 @@ fn main() {
         resizable: false,
         force_secondary_adapter: false,
     });
-    match version {
-        0 => context.run(app_hello::App::new()),
-        1 => context.run(app_game_no_menu::App::new()),
-        2 => context.run(app_game_component::App::new()),
-        3 => context.run(app_game_menu::App::new()),
-        4 => context.run(app_game_menu_cf::app()),
-        5 => context.run(app_game_menu_cf_boxed::app()),
-        _ => panic!("unexpected version"),
+    context.run(component)
+}
+
+fn run_terminal<C>(component: C)
+where
+    C: 'static + Component<State = (), Output = app::Output>,
+{
+    use chargrid_ansi_terminal::*;
+    let context = Context::new().unwrap();
+    context.run(component, col_encode::FromTermInfoRgb)
+}
+
+fn main() {
+    let Args { version, terminal } = Args::parser().with_help_default().parse_env_or_exit();
+    if terminal {
+        match version {
+            0 => run_terminal(app_hello::App::new()),
+            1 => run_terminal(app_game_no_menu::App::new()),
+            2 => run_terminal(app_game_component::App::new()),
+            3 => run_terminal(app_game_menu::App::new()),
+            4 => run_terminal(app_game_menu_cf::app()),
+            5 => run_terminal(app_game_menu_cf_boxed::app()),
+            _ => panic!("unexpected version"),
+        }
+    } else {
+        match version {
+            0 => run_wgpu(app_hello::App::new()),
+            1 => run_wgpu(app_game_no_menu::App::new()),
+            2 => run_wgpu(app_game_component::App::new()),
+            3 => run_wgpu(app_game_menu::App::new()),
+            4 => run_wgpu(app_game_menu_cf::app()),
+            5 => run_wgpu(app_game_menu_cf_boxed::app()),
+            _ => panic!("unexpected version"),
+        }
     }
 }
